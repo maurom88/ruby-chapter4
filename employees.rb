6f33a2a724ae4dc6313b2603dd5d0f552b1a8859
Mauro Meden <mauro.meden@shopify.com>
2021-07-11 08:39:32 -0600

    Add initialize method in superclass and call it with super

--- employees.rb
+++ employees.rb
@@ -2,6 +2,10 @@ class Employee
 
     attr_reader :name
 
+    def initialize(name)
+        self.name = name
+    end
+
     def name=(name)
         #code to validate and set name
         @name = name
@@ -16,6 +20,11 @@ end
 class SalariedEmployee < Employee
 
     attr_reader :salary
+
+    def initialize(name = "Anonymous", salary = 0.0)
+        super(name)
+        self.salary = salary
+    end
 
     def salary=(salary)
         #code to validate and set salary
@@ -35,6 +44,12 @@ class HourlyEmployee < Employee
 
     attr_reader :hourly_wage, :hours_per_week
 
+    def initialize(name = "Anonymous", hourly_wage = 0.0, hours_per_week = 0.0)
+        super(name)
+        self.hourly_wage = hourly_wage
+        self.hours_per_week = hours_per_week
+    end
+
     def hourly_wage=(hourly_wage)
         @hourly_wage = hourly_wage
     end
@@ -52,13 +67,13 @@ class HourlyEmployee < Employee
 
 end
 
-salaried_employee = SalariedEmployee.new
-salaried_employee.name = "Jane Doe"
-salaried_employee.salary = 50000
+salaried_employee = SalariedEmployee.new("Jane Doe", 50000)
+# salaried_employee.name = "Jane Doe"
+# salaried_employee.salary = 50000
 salaried_employee.print_pay_stub
 
-hourly_emplyoyee = HourlyEmployee.new
-hourly_emplyoyee.name = "John Smith"
-hourly_emplyoyee.hourly_wage = 14.97
-hourly_emplyoyee.hours_per_week = 30
+hourly_emplyoyee = HourlyEmployee.new("John Smith", 14.97, 30)
+# hourly_emplyoyee.name = "John Smith"
+# hourly_emplyoyee.hourly_wage = 14.97
+# hourly_emplyoyee.hours_per_week = 30
 hourly_emplyoyee.print_pay_stub
