89c690293393d920b1e8f21f5ddf86976dc1692d
Mauro Meden <mauro.meden@shopify.com>
2021-07-10 22:02:59 -0600

    Add HourlyEmployee class

--- employees.rb
+++ employees.rb
@@ -31,7 +31,34 @@ class SalariedEmployee < Employee
 
 end
 
+class HourlyEmployee < Employee
+
+    attr_reader :hourly_wage, :hours_per_week
+
+    def hourly_wage=(hourly_wage)
+        @hourly_wage = hourly_wage
+    end
+
+    def hours_per_week=(hours_per_week)
+        @hours_per_week = hours_per_week
+    end
+
+    def print_pay_stub
+        print_name
+        pay_for_period = hourly_wage * hours_per_week * 2
+        formatted_pay = format("$%.2f", pay_for_period)
+        puts "Pay this period: #{formatted_pay}"
+    end
+
+end
+
 salaried_employee = SalariedEmployee.new
 salaried_employee.name = "Jane Doe"
 salaried_employee.salary = 50000
 salaried_employee.print_pay_stub
+
+hourly_emplyoyee = HourlyEmployee.new
+hourly_emplyoyee.name = "John Smith"
+hourly_emplyoyee.hourly_wage = 14.97
+hourly_emplyoyee.hours_per_week = 30
+hourly_emplyoyee.print_pay_stub
